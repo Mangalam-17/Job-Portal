05ff979d7cc09ba3b117c4c0c7f624144fd93582
Mangalam Mishra <mangalamab17@gmail.com>
2025-10-07 10:28:00 +0530

    fixed routes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,12 @@ const connectDB = require("./config/database");
 
 connectDB();
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/user", userRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
